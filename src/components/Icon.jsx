be42d5a17f6b8c980769e8c0444a4f270da12d9b
yoshie <90372442+yoshility@users.noreply.github.com>
2022-03-10 17:25:06 +0900

    Creare MemoEditScreen and MemoCreateScreen

---
+++ src/components/Icon.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+import React from 'react';
+import { createIconSetFromIcoMoon } from '@expo/vector-icons';
+
+import icomoon from '../../assets/fonts/icomoon.ttf';
+import selection from '../../assets/fonts/selection.json';
+
+export default function Icon(props) {
+  const { name, size, color } = props;
+  const CustomIcon = createIconSetFromIcoMoon(selection);
+  return <CustomIcon name={name} size={size} color={color} />;
+}
+
+// 69番目の動画の途中で一旦ストップ
