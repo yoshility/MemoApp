705511fcaa2cb9fef4921866e0ce7432f5150083
yoshie <90372442+yoshility@users.noreply.github.com>
2022-03-18 02:05:01 +0900

    Improve ux

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -8,6 +8,7 @@ import firebase from 'firebase';
 
 import Button from '../components/Button';
 import Loading from '../components/Loading';
+import { translateErrors } from '../utils';
 
 export default function LogInScreen(props) {
   const { navigation } = props;
@@ -41,7 +42,8 @@ export default function LogInScreen(props) {
         });
       })
       .catch((error) => {
-        Alert.alert(error.code);
+        const errorMsg = translateErrors(error.code);
+        Alert.alert(errorMsg.title, errorMsg.description);
       })
       .then(() => {
         setLoading(false);
