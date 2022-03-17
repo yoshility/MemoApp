3c094a4a4e0743c6793f5641158449c31a3a672a
yoshie <90372442+yoshility@users.noreply.github.com>
2022-03-17 23:28:42 +0900

    Fetch data from firestore on MemoDetailScreen

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -10,6 +10,8 @@ import {
   shape, string, instanceOf, arrayOf,
 } from 'prop-types';
 
+import { dateToString } from '../utils';
+
 export default function MemoList(props) {
   const { memos } = props;
   const navigation = useNavigation();
@@ -18,11 +20,11 @@ export default function MemoList(props) {
     return (
       <TouchableOpacity
         style={styles.memoListItem}
-        onPress={() => { navigation.navigate('MemoDetail'); }}
+        onPress={() => { navigation.navigate('MemoDetail', { id: item.id }); }}
       >
         <View>
           <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
-          <Text style={styles.memoListItemDate}>{String(item.updatedAt)}</Text>
+          <Text style={styles.memoListItemDate}>{dateToString(item.updatedAt)}</Text>
         </View>
         <TouchableOpacity
           style={styles.memoDelete}
---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+import { format } from 'date-fns';
+
+export function dateToString(date) {
+  if (!date) { return ''; }
+  return format(date, 'yyyy年M月d日 HH時mm分');
+}
